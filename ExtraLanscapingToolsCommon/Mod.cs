d299a731e67da379b4cdc85d2ba145cf1b6dded3
earalov <earalov@thumbtack.net>
2016-10-22 22:49:26 +0600

    Add solution file

---
+++ ExtraLanscapingToolsCommon/Mod.cs
@@ -0,0 +1,11 @@
+﻿using ICities;
+
+namespace ExtraLanscapingToolsCommon
+{
+    public class Mod : IUserMod
+    {
+        public string Name => "ExtraLanscapingToolsCommon";
+
+        public string Description => "ExtraLanscapingToolsCommon";
+    }
+}
