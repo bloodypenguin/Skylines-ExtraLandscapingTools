f3f8ba73306d64efc6b647d036c81af1abf5a765
TPBCS <tpbsworkshop@gmail.com>
2018-04-19 20:49:59 +0200

    Added Terrain Panel, Water Panel and Resources Pa- nel to Theme Editor. Fixed a bug in NaturalResour- ceBrush.CopySprite when fetching sprites from InM- apEditor atlas. Fixed a bug in NaturalResourceBru- sh.SetupToolsbars that was trying to get GameMain- Panel when it doesnt exist. Changed to ThemeEdito- rMainPanel when in Theme Editor and to AssetEdito- rMainPanel when in Asset Editor.

--- NaturalResourcesBrush/Detours/TerrainPanelDetour.cs
+++ NaturalResourcesBrush/Detours/TerrainPanelDetour.cs
@@ -58,43 +58,104 @@ namespace NaturalResourcesBrush.Detours
             var panel = (TerrainPanel)Convert.ChangeType(this, typeof(TerrainPanel));
             for (index = 0; index < TerrainPanelDetour.kTools.Length; ++index)
                 SpawnEntry(panel, TerrainPanelDetour.kTools[index].enumName, index );
-            var ditchButton = (UIButton)SpawnEntry(panel, "Ditch", index );
+            var ditchButton = (UIButton)SpawnEntry(panel, "Ditch", ++index );
             ditchButton.atlas = Util.CreateAtlasFromEmbeddedResources(Assembly.GetExecutingAssembly(), "NaturalResourcesBrush.resources", new List<string> { "TerrainDitch" });
+            if(ToolsModifierControl.toolController.m_mode == ItemClass.Availability.ThemeEditor)
+            {
+                var sandButton = (UIButton)SpawnEntry(panel, "Sand", index);
+                sandButton.atlas = UIView.GetAView().defaultAtlas;
+                sandButton.normalFgSprite = "ResourceSand";
+                sandButton.hoveredFgSprite = "ResourceSandHovered";
+                sandButton.pressedFgSprite = "ResourceSandPressed";
+                sandButton.focusedFgSprite = "ResourceSandFocused";
+            }
         }
 
-        [RedirectMethod]
+        [RedirectMethod]        
         protected override void OnButtonClicked(UIComponent comp)
         {
-            int zOrder = comp.zOrder;
-            TerrainTool terrainTool = ToolsModifierControl.SetTool<TerrainTool>();
-            if (terrainTool == null)
+            if (ToolsModifierControl.toolController.m_mode == ItemClass.Availability.ThemeEditor)
             {
-                return;
-            }
-            var panel = (TerrainPanel)Convert.ChangeType(this, typeof(TerrainPanel));
-            ShowUndoTerrainOptionsPanel(panel, true);
-            ShowBrushOptionsPanel(panel, true);
-            //begin mod
-            UIView.library.Show("LandscapingInfoPanel");
-            //end mod
-            if (zOrder == 1 || zOrder == 3)
-                ShowLevelHeightPanel(panel, true);
-            else
-                ShowLevelHeightPanel(panel, false);
-            //begin mod
-            if (zOrder < kTools.Length)
-            {
-                terrainTool.m_mode = TerrainPanelDetour.kTools[zOrder].enumValue;
-                TerrainToolDetour.isDitch = false;
-            }
-            else
-            {
-                terrainTool.m_mode = TerrainTool.Mode.Shift;
-                TerrainToolDetour.isDitch = true;
-            }
-            //end mod
-        }
+                int zOrder = comp.zOrder;
+                TerrainTool terrainTool = null;
+                ResourceTool resourceTool = null;
+                var panel = (TerrainPanel)Convert.ChangeType(this, typeof(TerrainPanel));
+                if (zOrder < kTools.Length + 1)
+                {
+                    terrainTool = ToolsModifierControl.SetTool<TerrainTool>();
+                    if (terrainTool == null)
+                    {
+                        return;
+                    }
+                    ShowUndoTerrainOptionsPanel(panel, true);
+                    UIView.library.Show("LandscapingInfoPanel");
+                }
+                else
+                {
+                    resourceTool = ToolsModifierControl.SetTool<ResourceTool>();
+                    if (resourceTool == null)
+                        return;
+                    UIView.library.Hide("LandscapingInfoPanel");
+                    ShowUndoTerrainOptionsPanel(panel, false);
+                }
+                ShowBrushOptionsPanel(panel, true);
 
+                if (zOrder == 1 || zOrder == 3)
+                    ShowLevelHeightPanel(panel, true);
+                else
+                    ShowLevelHeightPanel(panel, false);
+                //begin mod
+                if (zOrder < kTools.Length)
+                {
+                    terrainTool.m_mode = kTools[zOrder].enumValue;
+                    TerrainToolDetour.isDitch = false;
+                }
+                else
+                {
+                    if (zOrder < kTools.Length + 1)
+                    {
+                        terrainTool.m_mode = TerrainTool.Mode.Shift;
+                        TerrainToolDetour.isDitch = true;
+                    }
+                    else
+                    {
+                        resourceTool.m_resource = NaturalResourceManager.Resource.Sand;
+                    }
+                }
+                //end mod
+            }
+            else
+            {
+                int zOrder = comp.zOrder;
+                TerrainTool terrainTool = ToolsModifierControl.SetTool<TerrainTool>();
+                if (terrainTool == null)
+                {
+                    return;
+                }
+                var panel = (TerrainPanel)Convert.ChangeType(this, typeof(TerrainPanel));
+                ShowUndoTerrainOptionsPanel(panel, true);
+                ShowBrushOptionsPanel(panel, true);
+                //begin mod
+                UIView.library.Show("LandscapingInfoPanel");
+                //end mod
+                if (zOrder == 1 || zOrder == 3)
+                    ShowLevelHeightPanel(panel, true);
+                else
+                    ShowLevelHeightPanel(panel, false);
+                //begin mod
+                if (zOrder < kTools.Length)
+                {
+                    terrainTool.m_mode = TerrainPanelDetour.kTools[zOrder].enumValue;
+                    TerrainToolDetour.isDitch = false;
+                }
+                else
+                {
+                    terrainTool.m_mode = TerrainTool.Mode.Shift;
+                    TerrainToolDetour.isDitch = true;
+                }
+                //end mod
+            }
+        }
         public override ItemClass.Service service { get; }
     }
 }
